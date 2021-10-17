31991709d646fcea5d33ed427bb2d8048a8b3408
“SakodaShiho” <sawadasakura322@gmail.com>
2021-10-18 00:01:08 +0900

    App.vueを修正してみる

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,48 @@
 <template>
-  <div id="app">
+  <!-- <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div> -->
+
+  <div>
+    <myheader></myheader>
+    <p v-if="msg.length > 0">
+      {{ msg }}
+    </p>
+    <p v-else>
+      no text
+    </p>
+    <input type="text" v-model="msg" />
+    <button @click="clear()">clear</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+
+// export default {
+//   name: 'App',
+//   components: {
+//     HelloWorld
+//   }
+// }
+import myheader from './components/myheader';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    myheader,
+  },
+  data() {
+    return {
+      msg: 'Hello World!',
+    };
+  },
+  methods: {
+    clear() {
+      this.msg = '';
+    },
+  },
+};
 </script>
 
 <style>
